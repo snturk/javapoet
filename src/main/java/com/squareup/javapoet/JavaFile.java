e7b1495d83ac275ce41f9a68ffe90b230d08ab31
Muratcan Şentürk <muratcan.senturk@ozguryazilim.com.tr>
2023-01-27 22:24:09 +0300

    fixed new line comments bug

--- src/main/java/com/squareup/javapoet/JavaFile.java
+++ src/main/java/com/squareup/javapoet/JavaFile.java
@@ -300,7 +300,10 @@ public final class JavaFile {
     }
 
     public Builder addTypeComment(String format, Object... args) {
-      this.typeComment.add(format + "\n", args);
+      if (!typeComment.formatParts.isEmpty()) {
+        typeComment.add("\n");
+      }
+      this.typeComment.add(format, args);
       return this;
     }
 
